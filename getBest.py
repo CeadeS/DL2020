da68e2106fd064f51c1ac1e297c19cb542e5944b
Constantin Hütterer <constantin.huetterer@vipcon.com>
2021-01-04 17:55:40 +0100

    adjust the "leader-score-script" to read all csv files in the project

--- getBest.py
+++ getBest.py
@@ -1,27 +1,30 @@
 import glob
 import csv
 import re
+from pathlib import Path
 
-# csvFileList = glob.glob("./**/*.csv") # check all csv file under root recursively
-csvFileList = glob.glob("./results/*.csv") # only results folder
-fNameRegex = re.compile(r'(?:.*/)(.*)(?:\.csv$)')
+csvFileList = Path('./').rglob('*.csv')
+fNameRegex = re.compile(r'(?:.*/)?(.*)(?:\.csv$)')
 showLines = (1,5,20)
 bestList = {}
 for k in showLines:
     bestList[k] = []
 
 for filePath in csvFileList:
-    fName = fNameRegex.search(filePath).groups()[0]
+    fName = fNameRegex.search(filePath.name).groups()[0]
     # print('=== FILE:', fName," ===")
-    with open(filePath, 'r') as file:
+    with open(filePath.absolute(), 'r') as file:
         csvreader = csv.reader(file, delimiter=',')
         for row in csvreader:
-            if row[0].isdecimal() and int(row[0])+1 in showLines:
-                # print(int(row[0])+1, ',', row[3])
-                bestList[int(row[0])+1].append((float(row[3]),fName,))
+            try:
+                if row[0].isdecimal() and int(row[0])+1 in showLines:
+                    # print(int(row[0])+1, ',', row[3])
+                    bestList[int(row[0])+1].append((float(row[3]),fName,))
+            except:
+                print('Error in file:', fName, ' - Not all lines from this file were processed.')
 
 # pretty inefficient, but easier to type
-showMax = 5
+showMax = 10
 for k in showLines:
     bestList[k].sort(key=lambda x: x[0], reverse=True)
     print(f'\n=== TOP {showMax} in {k} EPOCH(S) ===')
