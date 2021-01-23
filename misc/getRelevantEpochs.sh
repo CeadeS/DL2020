d40df2cf4b257d44b196701965c421563b3d4215
Constantin Hütterer <constantin.huetterer@vipcon.com>
2021-01-23 15:20:54 +0100

    let getRelevantEpochs.sh only output filename and accuracies for epochs

--- misc/getRelevantEpochs.sh
+++ misc/getRelevantEpochs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 for filename in "$@";do
 	echo $filename
-	awk '(NR==1 || NR==6 || NR==21 || NR==81 || NR==201)' $filename 
+	awk 'BEGIN {FS=","} NR==5 || NR==9 {print $4}' $filename
+	#awk 'BEGIN {FS=","} NR==1 || NR==5 || NR==9 {print $1,$4}' $filename # also show epoch
+	#awk '(NR==1 || NR==6 || NR==21 || NR==81 || NR==201)' $filename # show complete line
 done
